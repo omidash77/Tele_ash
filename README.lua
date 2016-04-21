06c33a58f258e261d6c6695bc1960d9473aa4c1c
omidash77 <omidalamzade77@gmail.com>
2016-04-21 01:00:24 -0700

    Update and rename .gitignore to README.lua

---
+++ README.lua
@@ -0,0 +1,17 @@
+sudo apt-get update
+
+sudo apt-get upgrade
+
+sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev make unzip git redis-server g++ libjansson-dev libpython-dev expat libexpat1-dev
+
+cd $HOME
+
+git clone https://github.com/omidash77/tele_ash
+
+cd tele_ash
+
+chmod +x launch.sh
+
+./launch.sh install
+
+./launch.sh
